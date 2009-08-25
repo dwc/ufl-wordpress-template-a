573a39ba438eac22b0a872710e5e517a09d1fa9f
spooner <spooner@10714c64-c2fd-11de-b153-b3ebe5430020>
2009-08-25 20:14:14 +0000

    Adding in navigation

--- themes/uf-template-a/trunk/archive.php
+++ themes/uf-template-a/trunk/archive.php
@@ -3,21 +3,22 @@
 	<div id="content">
 <?php     $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 <?php     /* If this is a category archive */ if (is_category()) { ?>				
-		<h2 id="secHeader" title="<?php ?>"><span></span><?php echo single_cat_title(); ?> Archive</h2>
+		<div id="secHeader" title="<?php ?>"><span></span><h2><?php echo single_cat_title(); ?> Archive</h2></div>
 <?php     /* If this is a daily archive */ } elseif (is_day()) { ?>
-		<h2 id="secHeader" title="<?php ?>"><span></span><?php /* http://bugs.webadmin.ufl.edu/357 */ the_time(get_option('date_format')); ?> Archive</h2>
+		<div id="secHeader" title="<?php ?>"><span></span><h2><?php /* http://bugs.webadmin.ufl.edu/357 */ the_time(get_option('date_format')); ?> Archive</h2></div>
 <?php     /* If this is a monthly archive */ } elseif (is_month()) { ?>
-		<h2 id="secHeader" title="<?php ?>"><span></span><?php the_time('F Y'); ?> Archive</h2>
+		<div id="secHeader" title="<?php ?>"><span></span><h2><?php the_time('F Y'); ?> Archive</h2></div>
 <?php     /* If this is a yearly archive */ } elseif (is_year()) { ?>
-		<h2 id="secHeader" title="<?php ?>"><span></span> <?php the_time('Y'); ?> Archive</h2>
+		<div id="secHeader" title="<?php ?>"><span></span><h2> <?php the_time('Y'); ?> Archive</h2></div>
 <?php     /* If this is a search */ } elseif (is_search()) { ?>
-		<h2 id="secHeader" title="<?php ?>"><span></span>Search Results</h2>
+		<div id="secHeader" title="<?php ?>"><span></span><h2>Search Results</h2></div>
 <?php     /* If this is an author archive */ } elseif (is_author()) { ?>
-		<h2 id="secHeader" title="<?php ?>"><span></span>Author Archive</h2>
+		<div id="secHeader" title="<?php ?>"><span></span><h2>Author Archive</h2></div>
 <?php     /* If this is a paged archive */ } elseif (isset($_GET['paged']) && ! empty($_GET['paged'])) { ?>
-		<h2 id="secHeader" title="<?php ?>"><span></span>Archives</h2>
+		<div id="secHeader" title="<?php ?>"><span></span><h2>Archives</h2></div>
 <?php     } ?>
 
+<!--
 <?php     $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 <?php     /* If this is a category archive */ if (is_category()) { ?>				
 		<h3  title="<?php ?>"><?php echo single_cat_title(); ?> Archive</h3>
@@ -34,32 +35,27 @@
 <?php     /* If this is a paged archive */ } elseif (isset($_GET['paged']) && ! empty($_GET['paged'])) { ?>
 		<h3  title="<?php ?>">Archives</h3>
 <?php     } ?>
+-->
 
-		<div class="archiveNav">
-			<?php next_posts_link('&laquo; Previous Stories'); ?><br />
-			<?php previous_posts_link('Next Stories &raquo;'); ?>
-		</div>
+		   <ul class="navigation">
+		       <li class="alignleft"><?php next_posts_link('&laquo; Previous '); ?></li><li class="alignright"><?php previous_posts_link('Next  &raquo;'); ?></li>
+		   </ul>
+
 
 <?php     while (have_posts()): the_post(); ?>
 		<div class="post">
 			<h3><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
-<?php         if (in_category('Audio')): ?>
-<?php             include('includes/audio-player.php'); ?>
-<?php         endif; ?>
 			<div class="storycontent">
 				<?php the_excerpt(); ?>
-
-<?php         if (in_category('Multimedia')): ?>
-<?php             include('includes/media-package.php'); ?>
-<?php         endif; ?>
+				<div class="date">Filed under <?php the_category(', '); ?> on <?php /* http://bugs.webadmin.ufl.edu/357 */ the_time(get_option('date_format')); ?>. <?php edit_post_link('Edit this entry', '', '.'); ?></div>
 			</div>
 		</div>
 <?php     endwhile; ?>
 
-		<div class="archiveNav">
-			<?php next_posts_link('&laquo; Previous Stories'); ?><br />
-			<?php previous_posts_link('Next Stories &raquo;'); ?>
-		</div>
+		   <ul class="navigation">
+		       <li class="alignleft"><?php next_posts_link('&laquo; Previous '); ?></li><li class="alignright"><?php previous_posts_link('Next  &raquo;'); ?></li>
+		   </ul>
+
 	</div><!-- #content -->
 
 <?php     get_sidebar(); ?>
