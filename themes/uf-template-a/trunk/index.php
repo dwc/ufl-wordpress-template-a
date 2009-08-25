42a84ffca7f194259af5f6fd1e6525bd527ee760
spooner <spooner@10714c64-c2fd-11de-b153-b3ebe5430020>
2009-08-25 20:12:45 +0000

    Cleaning up date entry

--- themes/uf-template-a/trunk/index.php
+++ themes/uf-template-a/trunk/index.php
@@ -6,13 +6,13 @@ Template Name: Home Page
 <?php get_header(); ?>
 	<div id="content">
 <?php if (have_posts()): ?>
-                        <h2 id="secHeader" title="<?php echo bloginfo('title'); ?>"><span></span><?php echo bloginfo('title'); ?></h2>
+                        <div id="secHeader" title="<?php echo bloginfo('title'); ?>"><span></span><h2><?php echo bloginfo('title'); ?></h2></div>
 <?php     while(have_posts()): the_post(); ?>
 			<div class="post">
 				<h3><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
 				<div class="storycontent">
-                                <p><span class="date"><?php the_time('M'); ?> <span class="day"><?php the_time('d'); ?> </span></span></p>
 				<?php the_excerpt(); ?>
+				<div class="date">Filed under <?php the_category(', '); ?> on <?php /* http://bugs.webadmin.ufl.edu/357 */ the_time(get_option('date_format')); ?>. <?php edit_post_link('Edit this entry', '', '.'); ?></div>
 				</div>
 			</div>
 <?php     endwhile; ?>
