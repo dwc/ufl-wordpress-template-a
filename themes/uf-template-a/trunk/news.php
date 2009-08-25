f7e710771fcefd1fcfb1c9eaedd626af33d829fe
spooner <spooner@10714c64-c2fd-11de-b153-b3ebe5430020>
2009-08-25 20:11:55 +0000

    Handling of date and work on navigation

--- themes/uf-template-a/trunk/news.php
+++ themes/uf-template-a/trunk/news.php
@@ -7,20 +7,30 @@ $NEWS_PAGE_ID = $post->ID;
 ?>
 <?php get_header(); ?>
 	<div id="content">
-<?php $news_category_id = get_cat_ID("About CLAS"); ?>
 
 <?php $my_query = new WP_Query("cat_id=*"); ?>
+
 <?php if ($my_query->have_posts()): ?>
-                        <h2 id="secHeader" title="News"><span></span>News</h2>
+                        <div id="secHeader" title="News"><span></span><h2>News</h2></div>
+
+		   <ul class="navigation">
+		       <li class="alignleft"><?php next_posts_link('&laquo; Previous '); ?></li><li class="alignright"><?php previous_posts_link('Next  &raquo;'); ?></li>
+		   </ul>
+
 <?php     while ($my_query->have_posts()): $my_query->the_post(); ?>
 			<div class="post">
 				<h3><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
 				<div class="storycontent">
-                                <p><span class="date"><?php the_time('M'); ?> <span class="day"><?php the_time('d'); ?> </span></span></p>
 				<?php the_excerpt(); ?>
+				<div class="date">Filed under <?php the_category(', '); ?> on <?php /* http://bugs.webadmin.ufl.edu/357 */ the_time(get_option('date_format')); ?>. <?php edit_post_link('Edit this entry', '', '.'); ?></div>
 				</div>
 			</div>
 <?php     endwhile; ?>
+
+		   <ul class="navigation">
+		       <li class="alignleft"><?php next_posts_link('&laquo; Previous '); ?></li><li class="alignright"><?php previous_posts_link('Next  &raquo;'); ?></li>
+		   </ul>
+
 <?php else: ?>
 				<h2 id="secHeader" title="News"><span></span>News</h2>
                                 <h3>No News</h3>
